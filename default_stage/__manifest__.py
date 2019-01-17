7321b107ef256a10964635c4a66a7dab5ee2bb74
WALTER MORENO <WMORENO@TPCOCL.CL>
2019-01-17 18:14:05 -0300

    Modulo agregado default_stages

---
+++ default_stage/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Project Default Stage',
+    'summary': 'Default task stage projects from projects',
+    'version': '12.0.',
+    'category': 'Project',
+    'author': 'saas tools',
+    'website': 'https://saastools.xyz',
+    'images': ['static/description/project_default_stage.png'],
+    'maintainer': 'World 360° Services',
+    'license': 'AGPL-3',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_view.xml',
+        'data/project_data.xml',
+    ],
+    'installable': True,
+}
